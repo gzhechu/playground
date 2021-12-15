215460dc64012fa4706ff8ae364de1baa1c7fb21
Robin <gzhechu@gmail.com>
2021-12-16 01:52:32 +0800

    改为 MVC 模式

--- tetris/python/tetris.py
+++ tetris/python/tetris.py
@@ -3,17 +3,18 @@
 
 import random
 from enum import Enum
-from tkinter import Tk, Frame, Canvas, ALL, NW
+from tkinter import Tk, Frame, Canvas, ALL
 
 BOARD_WIDTH = 720
 BOARD_HEIGHT = 720
 DELAY = 600
-STEP = 6
-SIDE = 5
-
+STEP = 30
+SIDE = 28
 
 ZONE_WIDTH = 12
 ZONE_HEIGHT = 22
+NEXT_X = 14
+NEXT_Y = 1
 
 ZONE_TOP_PX = 30
 ZONE_LEFT_PX = (BOARD_WIDTH-STEP*ZONE_WIDTH)/2
@@ -58,16 +59,27 @@ class Direction(Enum):
     DOWN = 3
 
 
-class Model():
-    def __init__(self):
+class TetrisModel():
+    def __init__(self, w: int, h: int):
+        if w < 8:
+            raise(Exception("game zone width less then 8"))
+        if h < 8:
+            raise(Exception("game zone height less then 8"))
+        self.width = w
+        self.height = h
         self.game_zone = []
-        for i in range(ZONE_HEIGHT):
-            row = [9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 9]
+        for i in range(self.height):
+            row = [9, 9]
+            for w in range(self.width):
+                row.append(0)
+            row.extend([9, 9])
             self.game_zone.append(row)
-        row = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+        row = []
+        for w in range(self.width+4):
+            row.append(9)
         self.game_zone.append(row)
 
-        self.inGame = True
+        self.in_game = True
         self.moveX = 2
         self.moveY = 0
         self.shape_num = 0
@@ -77,8 +89,8 @@ class Model():
 
     def new_tetris(self):
         self.tetris_num = self.next_tetris
-        self.next_tetris = random.randint(0, 6)
-        self.moveX = int(ZONE_WIDTH / 2 - 2)
+        self.next_tetris = random.randint(0, len(T)-1)
+        self.moveX = int(self.width / 2 - 2)
         self.moveY = 0
 
     def collision(self, x: int, y: int, num=None):
@@ -93,7 +105,6 @@ class Model():
                     if (s[i][j] != s[i][j] + self.game_zone[i + y][j + x + 2]):
                         # print("collision True")
                         return True
-        # print("collision False")
         return False
 
     def move(self, d: Direction):
@@ -124,7 +135,7 @@ class Model():
 
     def overflow(self):
         ret = False
-        for i in range(2, ZONE_WIDTH + 2):
+        for i in range(2, self.width + 2):
             if self.game_zone[0][i] > 0:
                 ret = True
                 break
@@ -139,25 +150,25 @@ class Model():
                 if 0 != s[i][j]:
                     self.game_zone[i + y][j + x + 2] = self.tetris_num + 1
         if self.overflow():
-            self.inGame = False
+            self.in_game = False
 
     def melt_detect(self, n: int):
         cnt = 0
         ret = True
-        for i in range(ZONE_WIDTH+4):
+        for i in range(self.width + 4):
             if self.game_zone[n][i] > 0:
                 cnt += 1
-        if cnt < ZONE_WIDTH + 4:
+        if cnt < self.width + 4:
             ret = False
         return ret
 
     def melt_it(self, n: int):
         for i in range(n, 0, -1):
-            for j in range(ZONE_WIDTH + 4):
+            for j in range(self.width + 4):
                 self.game_zone[i][j] = self.game_zone[i - 1][j]
 
     def try_melt(self):
-        i = ZONE_HEIGHT-1
+        i = self.height - 1
         while i > 0:
             if self.melt_detect(i):
                 print("try_melt", i, self.game_zone[i])
@@ -167,32 +178,17 @@ class Model():
 
 
 class GameView(Canvas):
-    def __init__(self):
-        super().__init__(width=BOARD_WIDTH, height=BOARD_HEIGHT,
+    def __init__(self, w=BOARD_WIDTH, h=BOARD_HEIGHT):
+        super().__init__(width=w, height=h,
                          background="black", highlightthickness=0)
-        self.tetris = Model()
-        self.init_game()
-        self.pack()
+        self.nextX = NEXT_X
+        self.nextY = NEXT_Y
 
-    def init_game(self):
-        self.score = 0
-        self.nextX = 13
-        self.nextY = 1
-        self.color = "lightblue"
-        self.next_color = "lightblue"
-
-        self.create_objects()
-        self.bind_all("<Key>", self.on_key_pressed)
-        self.after(DELAY, self.on_timer)
-
-    def create_objects(self):
-        '''在画布上绘制对象'''
-        self.create_text(30, 10, text="Score: {0}".format(self.score),
-                         tag="score", fill="white")
+        self.create_text(30, 10, text="Score: 0", tag="score", fill="white")
         self.create_rectangle(ZONE_LEFT_PX, ZONE_TOP_PX, ZONE_LEFT_PX + ZONE_WIDTH * STEP,
                               ZONE_TOP_PX + ZONE_HEIGHT * STEP,
                               fill="#0f0f0f", width=0, tag="zone")
-        self.new_tetris()
+        self.pack()
 
     def draw_tile(self, x, y, color, tag):
         rx = ZONE_LEFT_PX + x * STEP
@@ -200,72 +196,16 @@ class GameView(Canvas):
         self.create_rectangle(rx, ry, rx + SIDE, ry + SIDE,
                               fill=color, width=0, tag=tag)
 
-    def draw_shape(self, x, y, color, s, tag):
-        for i in range(4):
-            for j in range(4):
-                if (0 != s[i][j]):
-                    self.draw_tile(x + j, y + i, color, tag)
-
-    def move_shape(self, save=False):
-        x = self.tetris.moveX
-        y = self.tetris.moveY
-        s = T[self.tetris.tetris_num][self.tetris.shape_num]
-        tag = "move"
+    def redraw_shape(self, x, y, color, shape, tag, save=False):
         dots = self.find_withtag(tag)
         for dot in dots:
             self.delete(dot)
         if save:
             tag = "save"
-        self.draw_shape(x, y, self.color, s, tag)
-
-    def on_key_pressed(self, e):
-        if not self.tetris.inGame:
-            return
-        key = e.keysym
-        # print("pressed", key)
-
-        PAUSE_CURSOR_KEY = ["p", "P"]
-        if key in PAUSE_CURSOR_KEY:
-            self.tetris.pause_move = not self.tetris.pause_move
-            print("pause_move", self.pause_move)
-
-        LEFT_CURSOR_KEY = ["Left", "s", "S"]
-        if key in LEFT_CURSOR_KEY and self.tetris.move(Direction.LEFT):
-            self.move_shape()
-
-        RIGHT_CURSOR_KEY = ["Right", "f", "F"]
-        if key in RIGHT_CURSOR_KEY and self.tetris.move(Direction.RIGTHT):
-            self.move_shape()
-
-        DOWN_CURSOR_KEY = ["Down", "d", "D"]
-        if key in DOWN_CURSOR_KEY and self.tetris.move(Direction.DOWN):
-            self.move_shape()
-
-        UP_CURSOR_KEY = ["Up", "j", "J", "e", "E"]
-        if key in UP_CURSOR_KEY and self.tetris.rotate():
-            self.move_shape()
-
-        ESCAPE_CURSOR_KEY = "Escape"
-        if key == ESCAPE_CURSOR_KEY:
-            self.game_over()
-
-        SPACE_CURSOR_KEY = "space"
-        if key == SPACE_CURSOR_KEY:
-            while self.tetris.move(Direction.DOWN):
-                self.move_shape()
-
-    def new_tetris(self):
-        print("board new_tetris ")
-        self.color = self.next_color
-        self.next_color = colors[random.randint(0, 100) % 8]
-        self.tetris.new_tetris()
-        self.draw_shape(self.tetris.moveX, self.tetris.moveY, self.color,
-                        T[self.tetris.tetris_num][self.tetris.shape_num], "move")
-        dots = self.find_withtag("next")
-        for dot in dots:
-            self.delete(dot)
-        self.draw_shape(self.nextX, self.nextY, self.next_color,
-                        T[self.tetris.next_tetris][0], "next")
+        for i in range(4):
+            for j in range(4):
+                if (0 != shape[i][j]):
+                    self.draw_tile(x + j, y + i, color, tag)
 
     def melt_tile(self, n):
         tag = "save"
@@ -278,56 +218,128 @@ class GameView(Canvas):
             if c[1] < n*STEP+ZONE_TOP_PX:
                 self.move(tile, 0, STEP)
 
+    def draw_score(self, s):
+        score = self.find_withtag("score")
+        self.itemconfigure(score, text="score: {0}".format(s))
+
+    def game_over(self, score):
+        '''删除画布上的所有信息，并显示游戏结束'''
+        self.tetris.in_game = False
+        self.delete(ALL)
+        self.create_text(self.winfo_width() / 2, self.winfo_height()/2,
+                         text="Game Over with score {0}".format(score), fill="white")
+
+
+class GameController():
+    def __init__(self, model, view):
+        self._model = model
+        self._view = view
+        self.next_color = "lightblue"
+        self.color = self.next_color
+        self.score = 0
+        self.nextX = 13
+        self.nextY = 1
+        self.new_tetris()
+
+    def on_key_pressed(self, e):
+        if not self._model.in_game:
+            return
+        key = e.keysym
+        # print("pressed", key)
+
+        PAUSE_CURSOR_KEY = ["p", "P"]
+        if key in PAUSE_CURSOR_KEY:
+            self._model.pause_move = not self._model.pause_move
+            print("pause_move", self.pause_move)
+
+        LEFT_CURSOR_KEY = ["Left", "s", "S"]
+        if key in LEFT_CURSOR_KEY and self._model.move(Direction.LEFT):
+            self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                    T[self._model.tetris_num][self._model.shape_num], "move")
+
+        RIGHT_CURSOR_KEY = ["Right", "f", "F"]
+        if key in RIGHT_CURSOR_KEY and self._model.move(Direction.RIGTHT):
+            self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                    T[self._model.tetris_num][self._model.shape_num], "move")
+
+        DOWN_CURSOR_KEY = ["Down", "d", "D"]
+        if key in DOWN_CURSOR_KEY and self._model.move(Direction.DOWN):
+            self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                    T[self._model.tetris_num][self._model.shape_num], "move")
+        UP_CURSOR_KEY = ["Up", "j", "J", "e", "E"]
+        if key in UP_CURSOR_KEY and self._model.rotate():
+            self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                    T[self._model.tetris_num][self._model.shape_num], "move")
+
+        ESCAPE_CURSOR_KEY = "Escape"
+        if key == ESCAPE_CURSOR_KEY:
+            self.game_over()
+
+        SPACE_CURSOR_KEY = "space"
+        if key == SPACE_CURSOR_KEY:
+            while self._model.move(Direction.DOWN):
+                self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                        T[self._model.tetris_num][self._model.shape_num], "move")
+
+    def on_timer(self):
+        # print("on_timer", self.color, self.next_color, self.tetris.next_tetris)
+        self._view.draw_score(self.score)
+        if self._model.in_game:
+            if not self._model.pause_move:
+                if self._model.move(Direction.DOWN):
+                    self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                            T[self._model.tetris_num][self._model.shape_num], "move")
+                else:
+                    self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                            T[self._model.tetris_num][self._model.shape_num], "move", save=True)
+                    self._model.save()
+                    self.try_melt()
+                    self.new_tetris()
+            self._view.after(DELAY, self.on_timer)
+        else:
+            self.game_over()
+
+    def new_tetris(self):
+        self.color = self.next_color
+        self.next_color = colors[random.randint(0, 100) % 8]
+        self._model.new_tetris()
+        self._view.redraw_shape(self._model.moveX, self._model.moveY, self.color,
+                                T[self._model.tetris_num][self._model.shape_num], "move")
+        self._view.redraw_shape(self.nextX, self.nextY, self.next_color,
+                                T[self._model.next_tetris][0], "next")
+
     def try_melt(self):
         i = ZONE_HEIGHT-1
         while i > 0:
-            if self.tetris.melt_detect(i):
-                print("try_melt", i, self.tetris.game_zone[i])
-                self.tetris.melt_it(i)
-                self.melt_tile(i)
+            if self._model.melt_detect(i):
+                print("try_melt", i, self._model.game_zone[i])
+                self._model.melt_it(i)
+                self._view.melt_tile(i)
                 self.score += 1
                 i += +1
             i -= 1
 
-    def on_timer(self):
-        # print("on_timer", self.color, self.next_color, self.tetris.next_tetris)
-        self.draw_score()
-        if self.tetris.inGame:
-            if not self.tetris.pause_move:
-                if self.tetris.collision(self.tetris.moveX, self.tetris.moveY+1):
-                    self.move_shape(save=True)
-                    self.tetris.save()
-                    self.try_melt()
-                    self.new_tetris()
-                else:
-                    self.tetris.moveY += 1
-                    self.move_shape()
-            self.after(DELAY, self.on_timer)
-        else:
-            self.game_over()
-
-    def draw_score(self):
-        '''记分牌'''
-        score = self.find_withtag("score")
-        self.itemconfigure(score, text="score: {0}".format(self.score))
-
     def game_over(self):
-        '''删除画布上的所有信息，并显示游戏结束'''
-        self.tetris.inGame = False
-        self.delete(ALL)
-        self.create_text(self.winfo_width() / 2, self.winfo_height()/2,
-                         text="Game Over with score {0}".format(self.score), fill="white")
-        self.after(1000, self._quit)
+        self._model.in_game = False
+        self._view.delete(ALL)
+        self._view.create_text(self._view.winfo_width() / 2, self._view.winfo_height()/2,
+                               text="Game Over with score {0}".format(self.score), fill="white")
+        self._view.after(1000, self._quit)
 
     def _quit(self):
-        self.quit()
+        self._view.quit()
 
 
 class TetrisGame(Frame):
     def __init__(self):
         super().__init__()
         self.master.title('TETRIS - 俄罗斯方块大作战')
-        self.board = GameView()
+        m = TetrisModel(ZONE_WIDTH, ZONE_HEIGHT)
+        v = GameView()
+        c = GameController(m, v)
+        v.bind_all("<Key>", c.on_key_pressed)
+        v.after(DELAY, c.on_timer)
+        self.board = v
         self.pack()
 
 
