012454606bf28d09c2e93261e0d217df44e22b85
hechu <hechu@yy.com>
2022-01-29 18:28:02 +0800

    start html version,

---
+++ tetris/html/game.js
@@ -0,0 +1,191 @@
+$(function () {
+    $("#speed_slider").slider({
+        min: 1,
+        max: 100,
+        change: function (event, ui) {
+            wait_interval = 1000 / parseInt($("#speed_slider").slider("value"));
+            $("#speed").val(ui.value);
+        },
+        slide: function (event, ui) {
+            wait_interval = 1000 / parseInt($("#speed_slider").slider("value"));
+            $("#speed").val(ui.value);
+        },
+        value: 50,
+    });
+    $("#speed").val($("#speed_slider").slider("value"));
+    wait_interval = 1000 / parseInt($("#speed_slider").slider("value"));
+    $("#play").button({
+        text: false,
+        icons: {
+            primary: "ui-icon-play"
+        }
+    }).click(function () {
+        var options;
+        if ($(this).text() === "play") {
+            // Un-pause the game
+            paused = false;
+
+            // if (game_started) {
+            //     play();
+            // } else {
+            //     startGame(parseInt($("#num_columns").val()),
+            //         parseInt($("#num_rows").val()));
+            // }
+            options = {
+                label: "pause",
+                icons: {
+                    primary: "ui-icon-pause"
+                }
+            };
+        } else {
+            paused = true;
+            options = {
+                label: "play",
+                icons: {
+                    primary: "ui-icon-play"
+                }
+            };
+        }
+        $(this).button("option", options);
+    });
+
+    $("#stop").button({
+        text: false,
+        icons: {
+            primary: "ui-icon-stop"
+        }
+    }).click(function () {
+        paused = true;
+        game_started = false;
+        enableSliders();
+        $("#play").button("option", {
+            label: "play",
+            icons: {
+                primary: "ui-icon-play"
+            }
+        });
+    });
+
+    $("#debug").button().click(function () {
+        debug();
+    });
+});
+
+
+function GameView(w, h, squareSize) {
+    this.height = h;
+    this.width = w;
+    this.squareSize = squareSize;
+    this.context = document.getElementById('grid').getContext("2d");
+}
+
+GameView.prototype.drawGrid = function () {
+    this.context.beginPath();
+    for (var x = 0, i = 0; i <= this.width; x += this.squareSize, i++) {
+        this.context.moveTo(x, 0);
+        this.context.lineTo(x, this.height * this.squareSize);
+    }
+    for (var y = 0, i = 0; i <= this.height; y += this.squareSize, i++) {
+        this.context.moveTo(0, y);
+        this.context.lineTo(this.width * this.squareSize, y);
+    }
+    this.context.strokeStyle = "#eee";
+    this.context.stroke();
+};
+
+GameView.prototype.fill_cell = function (x, y, color, clear) {
+    if (clear) {
+        this.context.clearRect(
+            (x * this.squareSize) + 1,
+            (y * this.squareSize) + 1,
+            this.squareSize - 2,
+            this.squareSize - 2);
+    }
+    else {
+        this.context.fillStyle = color;
+        this.context.fillRect(
+            (x * this.squareSize) + 1,
+            (y * this.squareSize) + 1,
+            this.squareSize - 2,
+            this.squareSize - 2);
+    }
+};
+
+GameView.prototype.draw_shape = function (x, y, shape, color, clear) {
+    for (var h = 0; h < shape.height; h++) {
+        for (var w = 0; w < shape.width; w++) {
+            // console.info("shape cell: ", h, w, (shape.shape[h] >> w) & 1);
+            if (shape.shape[h] >> w & 1) {
+                this.fill_cell(x + w, y + h, color, clear);
+            }
+        }
+    }
+};
+
+var COLORS = new Array();
+var model = new TetrisModel(10, 20);
+var game_view = new GameView(10, 20, 30);
+game_view.drawGrid();
+var wait_interval = parseInt($("#speed_slider").slider("value"));
+
+function enableSliders() {
+    // for (var i = 0; i < 6; i++)
+    //     game_view.draw_shape(i * 2, i * 3, T[i][0], "#00ff00")
+    s = model.get_tetris()
+    console.info(s)
+    game_view.draw_shape(model.move_x, model.move_y, s, "#00ff00", false)
+}
+
+
+function debug() {
+    console.info("debug", Date.now())
+    // s = model.get_tetris()
+    // game_view.draw_shape(model.move_x, model.move_y, s, "#00ff00", true)
+    // model.move(Direction.Down)
+    // s = model.get_tetris()
+    // // console.info(s)
+    // game_view.draw_shape(model.move_x, model.move_y, s, "#00ff00", false)
+    setTimeout(on_timer, wait_interval);
+}
+
+
+function update() {
+    console.info("update", Date.now())
+    // if (model.move(Direction.Down))
+    {
+        previous = model.previous()
+        px = previous.x
+        py = previous.y
+        s = previous.shape
+        game_view.draw_shape(px, py, s, "#00ff00", true)
+        s = model.get_tetris()
+        // console.info(s)
+        game_view.draw_shape(model.move_x, model.move_y, s, "#00ff00", false)
+    }
+}
+
+var start = new Date;
+
+function on_timer() {
+    console.info((new Date - start) / 1000 + " Seconds");
+
+    if (model.in_game) {
+        if (model.pause_move)
+            return;
+        if (model.move(Direction.Down)) {
+            this.update()
+        }
+        else {
+            // update(true)
+            model.save()
+            try_melt()
+            new_tetris()
+        }
+    }
+    // else {
+    //     this.game_over()
+    // }
+    setTimeout(on_timer, wait_interval);
+}
+
+
---
+++ tetris/html/tetris.js
@@ -0,0 +1,160 @@
+
+var T = new Array();
+T[0] = [                                                // I
+    { shape: [1, 1, 1, 1], width: 1, height: 4 },
+    { shape: [15], width: 4, height: 1 }];
+T[1] = [                                                // T
+    { shape: [2, 7], width: 3, height: 2 },
+    { shape: [2, 3, 2], width: 2, height: 3 },
+    { shape: [7, 2], width: 3, height: 2 },
+    { shape: [1, 3, 1], width: 2, height: 3 }];
+T[2] = [                                                // O
+    { shape: [3, 3], width: 2, height: 2 }];
+T[3] = [                                                // L
+    { shape: [2, 2, 3], width: 2, height: 3 },
+    { shape: [7, 4], width: 3, height: 2 },
+    { shape: [3, 1, 1], width: 2, height: 3 },
+    { shape: [1, 7], width: 3, height: 2 }];
+T[4] = [                                                // J
+    { shape: [7, 1], width: 3, height: 2 },
+    { shape: [1, 1, 3], width: 2, height: 3 },
+    { shape: [4, 7], width: 3, height: 2 },
+    { shape: [3, 2, 2], width: 2, height: 3 }];
+T[5] = [                                                // Z
+    { shape: [6, 3], width: 3, height: 2 },
+    { shape: [1, 3, 2], width: 2, height: 3 }];
+T[6] = [                                                // S
+    { shape: [3, 6], width: 3, height: 2 },
+    { shape: [2, 3, 1], width: 2, height: 3 }];
+
+
+function TetrisModel(w, h) {
+    this.width = w;
+    this.height = h;
+    this.rows_completed = 0;
+    this.count = 0;
+    this.tetris_idx = 0
+    this.shape_idx = 0
+    this.next_tetris = 5
+    this.pause_move = false
+    this.move_x = 3
+    this.move_y = 0
+    this.previous_x = this.move_x
+    this.previous_y = this.move_y
+    this.in_game = true
+
+    // The board is represented as an array of integers, one integer for each row.
+    this.grid = new Array(this.height);
+    for (var i = 0; i < this.height; i++) {
+        this.grid[i] = 0;
+    }
+    this.new_tetris()
+    console.info(this.width, this.height)
+}
+
+TetrisModel.prototype.get_tetris = function () {
+    // console.info("get_tetris", this.tetris_idx, this.shape_idx)
+    return T[this.tetris_idx][this.shape_idx]
+};
+
+TetrisModel.prototype.previous = function () {
+    // console.info("previous", this.tetris_idx, this.shape_idx)
+    return { 'x': this.previous_x, 'y': this.previous_y, 'shape': T[this.tetris_idx][this.shape_idx] }
+};
+
+TetrisModel.prototype.new_tetris = function () {
+    this.count += 1
+    this.tetris_idx = this.next_tetris
+    this.shape_idx = 0
+    this.next_tetris = Math.floor(Math.random() * T.length);
+};
+
+TetrisModel.prototype.collision_detect = function (x, y, idx) {
+    if (x < 0)
+        return true;
+    var s;
+    if (idx >= 0)
+        s = T[this.tetris_idx][idx]
+    else
+        s = T[this.tetris_idx][this.shape_idx]
+    if (x > this.width - s.width)
+        return true;
+    if (y > this.height - s.height)
+        return true;
+    for (var h = 0; h < s.height; h++)
+        if (s.shape << x & this.grid[y + h] != 0)
+            return true
+    return false;
+};
+
+const Direction = {
+    Up: 'Up',
+    Down: 'Down',
+    Left: 'Left',
+    Right: 'Right'
+};
+
+TetrisModel.prototype.move = function (val) {
+    ret = false;
+    this.previous_x = this.move_x
+    this.previous_y = this.move_y
+    if (val === Direction.Left) {
+        if (!this.collision_detect(this.move_x - 1, this.move_y, -1)) {
+            this.move_x -= 1
+            ret = true
+        }
+    }
+    else if (val === Direction.Right) {
+        if (!this.collision_detect(this.move_x + 1, this.move_y, -1)) {
+            this.move_x += 1
+            ret = true
+        }
+    }
+    else if (val === Direction.Down) {
+        if (!this.collision_detect(this.move_x, this.move_y + 1, -1)) {
+            this.move_y += 1
+            ret = true
+        }
+    }
+    return ret
+};
+
+// # def try_melt(self):
+// #     melted = []
+// #     h = self.height - 1
+// #     while h > 0:
+// #         if 1 << self.width <= self.grid[h] + 1:
+// #             # print("try_melt", h, grid[h])
+// #             melted.append(h)
+// #             for y in range(h, 0, -1):
+// #                 self.grid[y] = self.grid[y - 1]
+// #             h += +1
+// #         h -= 1
+// #     return melted
+
+TetrisModel.prototype.try_melt = function () {
+    melted = []
+    h = self.height - 1
+    while (h > 0) {
+        if (1 << self.width <= self.grid[h] + 1) {
+            // # print("try_melt", h, grid[h])
+            melted.append(h)
+            for y in range(h, 0, -1):
+                self.grid[y] = self.grid[y - 1]
+            h += +1
+        }
+        h -= 1
+    }
+    return melted
+}
+
+TetrisModel.prototype.save = function () {
+    x = this.move_x
+    y = this.move_y
+    s = T[this.tetris_idx][this.shape_idx]
+    for (var h = 0; h < s.height; h++) {
+        this.grid[h + y] = this.grid[h + y] | (s.shape[h] << x)
+    }
+    if (this.grid[0] > 0)
+        this.in_game = false
+};
